6ce7ef1ba7ca7cb362a8c54d32beae9320d7018e
Himansu-03 <168916197+Himansu-03@users.noreply.github.com>
2024-09-08 14:09:30 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,11 @@
+const apiKey = '66f5f6f49506eb354105473910e699b5';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
